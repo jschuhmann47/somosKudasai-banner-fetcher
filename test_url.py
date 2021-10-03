abb22b392b2b5c165469a4b0a77e3e5f303d9a4c
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2021-10-03 12:40:37 -0300

    arreglado los tests

--- test_url.py
+++ test_url.py
@@ -1,6 +1,13 @@
 import pytest
-from urlFetcher import *
+import urlFetcher
+
 
 def test_urlValidaDaFotoValida():
     assert urlFetcher.urlFoto("https://somoskudasai.com/noticias/cultura-otaku/aseguran-que-la-franquicia-de-the-idolmaster-esta-en-decadencia/") == "https://somoskudasai.com/wp-content/uploads/2021/10/image-8.png"
+
+def test_urlDelSitioPeroNoValida():
+    assert urlFetcher.urlFoto("https://somoskudasai.com/") == "x"
+
+def test_urlNoValida():
+    assert urlFetcher.urlFoto("https://google.com/") == "x"
     
